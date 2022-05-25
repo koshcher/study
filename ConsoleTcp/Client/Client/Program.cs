0f51e0699a63ee93869693ffca3d8d812bbfc71e
Roman Koshchei <romankoshchei@gmail.com>
2022-05-25 10:34:05 +0300

    classes hw

---
+++ ConsoleTcp/Client/Client/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                TcpClient client = new TcpClient();
+                client.Connect("127.0.0.1", 4006);
+                byte[] data = new byte[256];
+                StringBuilder response = new StringBuilder();
+                NetworkStream stream = client.GetStream();
+                do
+                {
+                    int bytes = stream.Read(data, 0, data.Length);
+                    response.Append(Encoding.UTF8.GetString(data, 0, bytes));
+                } while (stream.DataAvailable);
+
+                Console.WriteLine(response.ToString());
+                string message = String.Format("В {0} от {1} получена строка: Привет, сервер!", DateTime.Now.ToShortTimeString(), "127.0.0.1");
+                //data = new byte[256];
+                data = Encoding.UTF8.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+                Console.WriteLine("Сообщение отправлено");
+                stream.Close();
+                client.Close();
+            }
+            catch(SocketException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleTcp/Server/Server/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ep = new IPEndPoint(ip, 4006);
+
+            TcpListener server = new TcpListener(ep);
+            try
+            {
+                server.Start();
+                while (true)
+                {
+                    Console.WriteLine("Ожидаем подключений....");
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Клиент Подключился ждем запрос");
+                    NetworkStream stream = client.GetStream();
+                    string message = String.Format("В {0} от {1} получена строка: Привет, клиент!", DateTime.Now.ToShortTimeString(), ip);
+                    byte[] data = Encoding.UTF8.GetBytes(message);
+                    stream.Write(data, 0, data.Length);
+                    Console.WriteLine("Сообщение отправлено");
+                    StringBuilder response = new StringBuilder();
+                    do
+                    {
+                        int bytes = stream.Read(data, 0, data.Length);
+                        response.Append(Encoding.UTF8.GetString(data, 0, bytes));
+                    } while (stream.DataAvailable);
+                    Console.WriteLine(response.ToString());
+                    stream.Close();
+                    client.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                server.Stop();
+            }
+            
+        }
+    }
+}
