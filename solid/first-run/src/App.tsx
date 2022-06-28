e774e35338cbb3b913775831443023601c1c7f6b
Roman Koshchei <romankoshchei@gmail.com>
2022-06-28 09:21:03 +0300

    solid first

---
+++ solid/first-run/src/App.tsx
@@ -0,0 +1,20 @@
+import type { Component } from 'solid-js';
+
+import logo from './logo.svg';
+import styles from './App.module.css';
+
+const App: Component = () => {
+  return (
+    <div class={styles.App}>
+      <header class={styles.header}>
+        <img src={logo} class={styles.logo} alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <h1>OMG</h1>
+      </header>
+    </div>
+  );
+};
+
+export default App;
