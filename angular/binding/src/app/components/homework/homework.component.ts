da0a80259e16ecaedad313498f58874612f7994f
Roman Koshchei <romankoshchei@gmail.com>
2022-06-29 10:41:21 +0300

    service hw

--- angular/binding/src/app/components/homework/homework.component.ts
+++ angular/binding/src/app/components/homework/homework.component.ts
@@ -11,13 +11,7 @@ export class HomeworkComponent implements OnInit {
 
   first = "";
   second = "";
-  change = () => {
-    // const tmp = this.first;
-    // this.first = this.second;
-    // this.second = tmp;
-    [this.first, this.second] = [this.second, this.first];
-
-  }
+  change = () => [this.first, this.second] = [this.second, this.first];
 
   inputCount = 0;
   res: string[] = [];
@@ -26,18 +20,17 @@ export class HomeworkComponent implements OnInit {
   }
 
   matrixStr = "";
-
   matrix: string[][] = []
   createMatrix() {
     let row = 0;
     this.matrix[row] = [];
+
     for (const str of this.matrixStr) {
       if (str == '\n') {
         row++;
         this.matrix[row] = [];
         continue;
       }
-
       this.matrix[row].push(str);
     }
   }
@@ -48,17 +41,43 @@ export class HomeworkComponent implements OnInit {
     rotate: 0
   }
 
+  colors: string[] = ['red', 'blue', 'yellow', 'brown']
+  getRandomInt(max: number) {
+    return Math.floor(Math.random() * max);
+  }
+
+  blocks: string[] = []
+  addBlock() {
+    this.blocks.push(this.colors[this.getRandomInt(this.colors.length)]);
+  }
+
+
   makeColor($event: MouseEvent) {
     let target = $event.target as Element;
     if (target != null && target.tagName == "TD") {
-      target.setAttribute('style', 'background-color: red');
+      target.setAttribute('style', `background-color: ${this.colors[this.getRandomInt(this.colors.length)]}`);
     }
   }
-
   unmakeColor($event: MouseEvent) {
     let target = $event.target as Element;
     if (target != null && target.tagName == "TD") {
       target.setAttribute('style', 'background-color: none');
+    }
+  }
+
+  mosts: any = {
+    car: "Car 'The American Dream' now measures 30.54 metres",
+    aircraft: "Aircraft 'Antonov An-225 Mriya'",
+    ship: "Ship 'Seawise Giant'"
+  }
+  most = "";
+  changeMost(type: string) {
+    this.most = this.mosts[type];
+  }
+  changeMostE(e: Event) {
+    const target = e.target as Element;
+    if (target != null && target.tagName == "BUTTON") {
+      this.most = this.mosts[target.innerHTML];
     }
   }
 
